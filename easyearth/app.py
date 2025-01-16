3112254749e357c35ae3eecd992c9213f4d4659f
Yan Cheng <chengyan2017@gmail.com>
2025-01-16 17:21:31 +0100

    update

--- easyearth/app.py
+++ easyearth/app.py
@@ -2,14 +2,6 @@ from easyearth import init_api
 
 app = init_api().app  # Create the app as a module-level variable
 
-@app.route('/')
-def index():
-    return "Welcome to Easy Earth API!"
-
-@app.route('/favicon.ico')
-def favicon():
-    return '', 204
-
 
 if __name__ == "__main__":
     app.run(port=3781)
