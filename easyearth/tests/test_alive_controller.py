881050f2e886e691c7065c609c81064b028c8f10
Yan Cheng <chengyan2017@gmail.com>
2025-03-31 17:46:21 +0200

    fix url

--- easyearth/tests/test_alive_controller.py
+++ easyearth/tests/test_alive_controller.py
@@ -7,11 +7,13 @@ from easyearth.tests import BaseTestCase
 
 class BaseTestAliveController(BaseTestCase):
     def test_get_alive(self):
-        response = self.client.open("/ping", method="GET")
+        response = self.client.open("/v1/easyearth/ping", method="GET")
+        print(f"Response: {response.data}")  # Add debug output
         self.assert200(response)
 
 
 if __name__ == "__main__":
+    # TODO: unittest cannot find urls, but the testing works when running the app first and enter the url on the web browser
     import unittest
 
     unittest.main()
